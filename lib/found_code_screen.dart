745434f592b698cf742102495354262e20eee72c
Vitaly <38248500+le-vitalique@users.noreply.github.com>
2024-11-01 12:35:53 +0400

    update lib/found_code_screen.dart

--- lib/found_code_screen.dart
+++ lib/found_code_screen.dart
@@ -1,15 +1,13 @@
-// Screen with scanned code value
 import 'package:flutter/material.dart';
 import 'package:testproject/contact.dart';
 
 class FoundCodeScreen extends StatefulWidget {
-  // final String value;
-  final List<Contact> value;
+  final List<Contact> contactsList;
   final Function() screenClosed;
 
   const FoundCodeScreen({
     super.key,
-    required this.value,
+    required this.contactsList,
     required this.screenClosed,
   });
 
@@ -18,19 +16,7 @@ class FoundCodeScreen extends StatefulWidget {
 }
 
 class _FoundCodeScreenState extends State<FoundCodeScreen> {
-  // List<Contact> contacts = getContacts();
-  // static List<Contact> getContacts() {
-  //   const data = [
-  //     {"id": 0, "name": "Rosalind Charles", "phone": "+1 (854) 436-2113"},
-  //     {"id": 1, "name": "Rosemarie Stark", "phone": "+1 (902) 559-3648"},
-  //     {"id": 2, "name": "Massey Parker", "phone": "+1 (887) 412-2951"},
-  //     {"id": 3, "name": "Koch Whitney", "phone": "+1 (918) 473-3084"},
-  //     {"id": 4, "name": "Padilla Wolfe", "phone": "+1 (931) 480-3519"}
-  //   ];
-  //   return data.map<Contact>(Contact.fromJson).toList();
-  // }
-
-  final List<GlobalKey<FormState>> _contactKeys = [];
+  final List<GlobalKey<FormState>> _contactFormStateKeys = [];
   final List<TextEditingController> _nameControllers = [];
   final List<TextEditingController> _phoneControllers = [];
 
@@ -43,15 +29,15 @@ class _FoundCodeScreenState extends State<FoundCodeScreen> {
   Widget buildUsers(List<Contact> contacts) => ListView.builder(
         itemCount: contacts.length,
         itemBuilder: (context, index) {
-          final contact = contacts[index];
+          final Contact contact = contacts[index];
 
-          _contactKeys.add(GlobalKey<FormState>());
+          _contactFormStateKeys.add(GlobalKey<FormState>());
 
           _nameControllers.add(TextEditingController(text: contact.name));
           _phoneControllers.add(TextEditingController(text: contact.phone));
 
           return Form(
-            key: _contactKeys[index],
+            key: _contactFormStateKeys[index],
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
@@ -84,7 +70,7 @@ class _FoundCodeScreenState extends State<FoundCodeScreen> {
                 IconButton(
                   key: const Key('submit'),
                   onPressed: () {
-                    _submitForm(_contactKeys[index]);
+                    _submitForm(_contactFormStateKeys[index]);
                   },
                   icon: const Icon(Icons.save),
                 ),
@@ -123,12 +109,11 @@ class _FoundCodeScreenState extends State<FoundCodeScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const Text('Scanned Code:'),
-                // Text(widget.value),
                 const SizedBox(
                   height: 20,
                 ),
                 Expanded(
-                  child: buildUsers(widget.value),
+                  child: buildUsers(widget.contactsList),
                 ),
               ],
             ),
