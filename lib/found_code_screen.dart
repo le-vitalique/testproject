c4b8817438cb62780c25d720be789df63cb9ab00
le-vitalique <site.lab7@gmail.com>
2024-11-01 09:55:23 +0400

    commit

--- lib/found_code_screen.dart
+++ lib/found_code_screen.dart
@@ -18,19 +18,18 @@ class FoundCodeScreen extends StatefulWidget {
 }
 
 class _FoundCodeScreenState extends State<FoundCodeScreen> {
-  List<Contact> contacts = getContacts();
 
-  static List<Contact> getContacts() {
-    const data = [
-      {"id": 0, "name": "Rosalind Charles", "phone": "+1 (854) 436-2113"},
-      {"id": 1, "name": "Rosemarie Stark", "phone": "+1 (902) 559-3648"},
-      {"id": 2, "name": "Massey Parker", "phone": "+1 (887) 412-2951"},
-      {"id": 3, "name": "Koch Whitney", "phone": "+1 (918) 473-3084"},
-      {"id": 4, "name": "Padilla Wolfe", "phone": "+1 (931) 480-3519"}
-    ];
-
-    return data.map<Contact>(Contact.fromJson).toList();
-  }
+  // List<Contact> contacts = getContacts();
+  // static List<Contact> getContacts() {
+  //   const data = [
+  //     {"id": 0, "name": "Rosalind Charles", "phone": "+1 (854) 436-2113"},
+  //     {"id": 1, "name": "Rosemarie Stark", "phone": "+1 (902) 559-3648"},
+  //     {"id": 2, "name": "Massey Parker", "phone": "+1 (887) 412-2951"},
+  //     {"id": 3, "name": "Koch Whitney", "phone": "+1 (918) 473-3084"},
+  //     {"id": 4, "name": "Padilla Wolfe", "phone": "+1 (931) 480-3519"}
+  //   ];
+  //   return data.map<Contact>(Contact.fromJson).toList();
+  // }
 
   Widget buildUsers(List<Contact> contacts) => ListView.builder(
         itemCount: contacts.length,
